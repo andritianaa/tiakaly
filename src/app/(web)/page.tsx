81d6b528f3b1716b462661da9327677897a6196e
andritianaa <andritianasteve@gmail.com>
2025-04-15 11:39:50 +0300

    Remove Testimonial component from RoutePage

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,6 +1,5 @@
 import FAQ from '@/app/(web)/components/faq';
 import RecentContent from '@/app/(web)/components/recent-content';
-import Testimonial from '@/app/(web)/components/testimonial';
 import { MouseAnimation } from '@/components/mouse-animation';
 
 import Hero from './components/hero';
@@ -12,7 +11,7 @@ export default async function RoutePage() {
       <MouseAnimation />
       <RecentContent />
       <FAQ />
-      <Testimonial />
+      {/* <Testimonial /> */}
     </>
   );
 }
