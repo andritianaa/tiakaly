5627dadfcc6ee763d74feb61c63b27aeafca7680
andritianaa <andritianasteve@gmail.com>
2025-05-12 10:52:09 +0300

    feat: integrate Sentry for error tracking and monitoring in Next.js application
    - Added Sentry configuration for edge features in `sentry.edge.config.ts`
    - Implemented Sentry configuration for server-side error tracking in `sentry.server.config.ts`
    - Created a sample API route to demonstrate error handling with Sentry in `src/app/api/sentry-example-api/route.ts`
    - Developed a global error component to capture exceptions and display a generic error page in `src/app/global-error.tsx`
    - Built a frontend page to trigger and send sample errors to Sentry in `src/app/sentry-example-page/page.tsx`
    - Configured client-side Sentry initialization with Replay integration in `src/instrumentation-client.ts`
    - Set up instrumentation for Sentry in `src/instrumentation.ts` to handle different runtimes

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://87e3922e26c5904358b0fd8cb2b5e8a1@o4508844163792896.ingest.de.sentry.io/4509288833220688",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
